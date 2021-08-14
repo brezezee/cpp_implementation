687469f9d5fdab88bf2ce56b8bdf122d6a98aef0
brezezee <18882025039@163.com>
2021-08-15 00:31:53 +0800

    update

--- TimeWheel/main.cpp
+++ TimeWheel/main.cpp
@@ -20,54 +20,54 @@ std::string timetoStr(){
 int main() {
   // Four level time wheels: Hour, Minute, Secon, Millisecond.
   int timer_step_ms = 50;
-  TimeWheelManager tws(timer_step_ms);
-  // tws.AppendTimeWheel(1000 / timer_step_ms, timer_step_ms, "MillisecondTimeWheel");
-  tws.AppendTimeWheel(60, 1000, "SecondTimeWheel");
+  TimeWheelManager tw_manager(timer_step_ms);
+  // tw_manager.AppendTimeWheel(1000 / timer_step_ms, timer_step_ms, "MillisecondTimeWheel");
+  tw_manager.AppendTimeWheel(60, 1000, "SecondTimeWheel");
   // Hour time wheel. 24 scales, 1 scale = 60 * 60 * 1000 ms.
-  tws.AppendTimeWheel(24, 60 * 60 * 1000, "HourTimeWheel");
+  tw_manager.AppendTimeWheel(24, 60 * 60 * 1000, "HourTimeWheel");
   // Minute time wheel. 60 scales, 1 scale = 60 * 1000 ms.
-  tws.AppendTimeWheel(60, 60 * 1000, "MinuteTimeWheel");
+  tw_manager.AppendTimeWheel(60, 60 * 1000, "MinuteTimeWheel");
   // Second time wheel. 60 scales, 1 scale = 1000 ms.
   
   // Millisecond time wheel. 1000/timer_step_ms scales, 1 scale = timer_step ms.
   
   
-  tws.Start();
+  tw_manager.Start();
 
-  tws.CreateTimerAt(GetNowTimestamp() + 10000, []() {
+  tw_manager.CreateTimerAt(GetNowTimestamp() + 10000, []() {
     std::cout << "At now+10s" << std::endl;
   });
 
-  tws.CreateTimerAfter(500, []() {
+  tw_manager.CreateTimerAfter(500, []() {
     std::cout << "After 0.5s" << std::endl;
   });
 
   std::cout << timetoStr() << std::endl;
-  auto timer_id = tws.CreateTimerEvery(5000, []() {
+  auto timer_id = tw_manager.CreateTimerEvery(5000, []() {
     std::cout << "Every 5s: " << timetoStr() << std::endl;
   });
 
-  tws.CreateTimerEvery(30 * 1000, []() {
+  tw_manager.CreateTimerEvery(30 * 1000, []() {
     std::cout << "Every 30s: " << timetoStr() <<std::endl;
   });
 
-  tws.CreateTimerEvery(60 * 1000, []() {
+  tw_manager.CreateTimerEvery(60 * 1000, []() {
     std::cout << "Every 1m: " << timetoStr() << std::endl;
   });
 
-  tws.CreateTimerEvery(90 * 1000, []() {
+  tw_manager.CreateTimerEvery(90 * 1000, []() {
     std::cout << "Every 1.5m: " << timetoStr() << std::endl;
   });
 
-  tws.CreateTimerEvery(120 * 1000, []() {
+  tw_manager.CreateTimerEvery(120 * 1000, []() {
     std::cout << "Every 2m: " << timetoStr() << std::endl;
   });
 
   std::this_thread::sleep_for(std::chrono::seconds(30));
-  tws.CancelTimer(timer_id);
+  tw_manager.CancelTimer(timer_id);
 
   std::this_thread::sleep_for(std::chrono::minutes(20));
-  tws.Stop();
+  tw_manager.Stop();
 
   return 0;
 }
